ac133530f14bb1412f056eb764f9900a5274d5f5
Rickert Mulder <circlingthesun@gmail.com>
2014-03-20 05:16:13 +0200

    feat(offcanvas): Added offcanvas support in a more "Angular-way" ;)

--- src/offcanvas/offcanvas.js
+++ src/offcanvas/offcanvas.js
@@ -3,45 +3,49 @@ angular.module("mm.foundation.offCanvas", [])
         return {
             restrict: 'C',
             link: function ($scope, element, attrs) {
-            	var win = angular.element($window);
-            	var sidebar = $scope.sidebar = element;
+                var win = angular.element($window);
+                var sidebar = $scope.sidebar = element;
 
-            	$scope.hide = function () {
-		            sidebar.removeClass('move-left');
-		            sidebar.removeClass('move-right');
-		        }
+                $scope.hide = function () {
+                    sidebar.removeClass('move-left');
+                    sidebar.removeClass('move-right');
+                };
 
-		        win.bind("resize.body", $scope.hide);
+                win.bind("resize.body", $scope.hide);
 
             },
             controller: ['$scope', function($scope) {
 
-	            this.leftToggle = function() {
-	                if($scope.sidebar.hasClass("move-right"))
-                    	$scope.sidebar.removeClass('move-right');
-                    else
-                    	$scope.sidebar.addClass("move-right");
-	            }
+                this.leftToggle = function() {
+                    if($scope.sidebar.hasClass("move-right")){
+                        $scope.sidebar.removeClass('move-right');
+                    }
+                    else{
+                        $scope.sidebar.addClass("move-right");
+                    }
+                };
 
-	            this.rightToggle = function() {
-	                if($scope.sidebar.hasClass("move-left"))
-                    	$scope.sidebar.removeClass('move-left');
-                    else
-                    	$scope.sidebar.addClass("move-left");
-	            }
+                this.rightToggle = function() {
+                    if($scope.sidebar.hasClass("move-left")){
+                        $scope.sidebar.removeClass('move-left');
+                    }
+                    else{
+                        $scope.sidebar.addClass("move-left");
+                    }
+                };
 
-	            this.hide = function() {
-	                $scope.hide();
-	            }
-        	}]
+                this.hide = function() {
+                    $scope.hide();
+                };
+            }]
         };
     }])
-	.directive('leftOffCanvasToggle', [function () {
+    .directive('leftOffCanvasToggle', [function () {
         return {
-        	require: '^offCanvasWrap',
+            require: '^offCanvasWrap',
             restrict: 'C',
             link: function ($scope, element, attrs, offCanvasWrap) {
-            	element.on('click', function () {
+                element.on('click', function () {
                     offCanvasWrap.leftToggle();
                 });
             }
@@ -49,22 +53,22 @@ angular.module("mm.foundation.offCanvas", [])
     }])
     .directive('rightOffCanvasToggle', [function () {
         return {
-        	require: '^offCanvasWrap',
+            require: '^offCanvasWrap',
             restrict: 'C',
             link: function ($scope, element, attrs, offCanvasWrap) {
-            	element.on('click', function () {
+                element.on('click', function () {
                     offCanvasWrap.rightToggle();
                 });
             }
         };
     }])
-   	.directive('exitOffCanvas', [function () {
+       .directive('exitOffCanvas', [function () {
         return {
-        	require: '^offCanvasWrap',
+            require: '^offCanvasWrap',
             restrict: 'C',
             link: function ($scope, element, attrs, offCanvasWrap) {
-            	console.log(element);
-            	element.on('click', function () {
+                console.log(element);
+                element.on('click', function () {
                     offCanvasWrap.hide();
                 });
             }
@@ -72,11 +76,11 @@ angular.module("mm.foundation.offCanvas", [])
     }])
     .directive('offCanvasList', [function () {
         return {
-        	require: '^offCanvasWrap',
+            require: '^offCanvasWrap',
             restrict: 'C',
             link: function ($scope, element, attrs, offCanvasWrap) {
-            	console.log(element);
-            	element.on('click', function () {
+                console.log(element);
+                element.on('click', function () {
                     offCanvasWrap.hide();
                 });
             }
