26e8c5a02ceb4da8bfd3885ce4f382e5a945756d
Jeff Browning <jeff@jkbrowning.com>
2014-04-06 18:09:03 -0400

    fix(dropdownToggle): Refactor test and fix initial hiding of dropdown target

--- src/dropdownToggle/dropdownToggle.js
+++ src/dropdownToggle/dropdownToggle.js
@@ -1,15 +1,14 @@
 /*
- * dropdownToggle - Provides dropdown menu functionality in place of bootstrap js
+ * dropdownToggle - Provides dropdown menu functionality
  * @restrict class or attribute
  * @example:
-   <li class="dropdown">
-     <a class="dropdown-toggle">My Dropdown Menu</a>
-     <ul class="dropdown-menu">
-       <li ng-repeat="choice in dropChoices">
-         <a ng-href="{{choice.href}}">{{choice.text}}</a>
-       </li>
-     </ul>
-   </li>
+
+   <a dropdown-toggle="#dropdown-menu">My Dropdown Menu</a>
+   <ul id="dropdown-menu" class="f-dropdown">
+     <li ng-repeat="choice in dropChoices">
+       <a ng-href="{{choice.href}}">{{choice.text}}</a>
+     </li>
+   </ul>
  */
 angular.module('mm.foundation.dropdownToggle', [ 'mm.foundation.position' ])
 
@@ -22,10 +21,11 @@ angular.module('mm.foundation.dropdownToggle', [ 'mm.foundation.position' ])
       dropdownToggle: '@'
     },
     link: function(scope, element, attrs) {
+      var dropdown = angular.element($document[0].querySelector(scope.dropdownToggle));
 
       scope.$watch('$location.path', function() { closeMenu(); });
-      element.bind('click', function (event) {
-        var dropdown = angular.element($document[0].querySelector(scope.dropdownToggle));
+      element.bind('click', function (event) { 
+        dropdown = angular.element($document[0].querySelector(scope.dropdownToggle));
         var elementWasOpen = (element === openElement);
 
         event.preventDefault();
@@ -60,6 +60,10 @@ angular.module('mm.foundation.dropdownToggle', [ 'mm.foundation.position' ])
           $document.bind('click', closeMenu);
         }
       });
+
+      if (dropdown) {
+        dropdown.css('display', 'none');
+      }
     }
   };
 }]);
