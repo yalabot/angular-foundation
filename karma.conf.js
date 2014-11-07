3c4af182d0c50a34f16a075b7d26536d5cb5adb4
Jeff Browning <jeff@jkbrowning.com>
2014-11-06 19:00:12 -0500

    chore(all): Upgrade Karma to 0.12.x

--- karma.conf.js
+++ karma.conf.js
@@ -1,55 +1,51 @@
+module.exports = function(config) {
+ config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '.',
 
-// base path, that will be used to resolve files and exclude
-basePath = '.';
+    frameworks: ['jasmine'],
+    // list of files / patterns to load in the browser
+    files: [
+      'misc/test-lib/jquery-1.8.2.min.js',
+      'misc/test-lib/angular.js',
+      'misc/test-lib/angular-mocks.js',
+      'misc/test-lib/helpers.js',
+      'src/**/*.js',
+      'template/**/*.js'
+    ],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'misc/test-lib/jquery-1.8.2.min.js',
-  'misc/test-lib/angular.js',
-  'misc/test-lib/angular-mocks.js',
-  'misc/test-lib/helpers.js',
-  'src/**/*.js',
-  'template/**/*.js'
-];
+    // list of files to exclude
+    exclude: [
+      'src/**/demo.js'
+    ],
 
-// list of files to exclude
-exclude = [
-];
+    browsers: [
+      'Chrome'
+    ],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari
-// - PhantomJS
-browsers = [
-  'Chrome'
-];
+    // test results reporter to use
+    // possible values: dots || progress
+    reporters: ['progress'],
 
-// test results reporter to use
-// possible values: dots || progress
-reporters = ['progress'];
+    // web server port
+    port: 9018,
 
-// web server port
-port = 9018;
+    // cli runner port
+    runnerPort: 9100,
 
-// cli runner port
-runnerPort = 9100;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+ });
+}
 
