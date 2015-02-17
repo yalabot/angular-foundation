2d66bc92f234b8a3770fc6c767f0bb83561af72f
Mike Angstadt <mjangstadt@gmail.com>
2015-02-17 06:18:18 -0600

    reverted previous commit

--- src/tabs/tabs.js
+++ src/tabs/tabs.js
@@ -193,10 +193,8 @@ angular.module('mm.foundation.tabs', [])
         if (attrs.active) {
           getActive = $parse(attrs.active);
           scope.active = getActive(scope.$parent);
-        } else {
--          setActive = getActive = angular.noop;
-         }
-
+        } 
+        
         scope.$watch('active', function(active) {
 
           if (active) {
