ad8fdce508501daf71210d8eb338b4f582d2578d
Mateusz <mateusz@coldrun.pl>
2015-11-25 13:02:21 +0100

    fix(topbar): incorrect unbind params in the ondestroy fn

--- src/topbar/topbar.js
+++ src/topbar/topbar.js
@@ -251,8 +251,8 @@ angular.module("mm.foundation.topbar", ['mm.foundation.mediaQueries'])
         angular.element($window).bind('scroll', onScroll);
 
         scope.$on('$destroy', function() {
-          angular.element($window).unbind('scroll', onResize);
-          angular.element($window).unbind('resize', onScroll);
+          angular.element($window).unbind('resize', onResize);
+          angular.element($window).unbind('scroll', onScroll);
         });
 
         if (topbarContainer.hasClass('fixed')) {
